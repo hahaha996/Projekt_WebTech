30296d4e3d362a55f9b4cacaf611b0f7d1db834e
hahaha996 <fatspear12@gmail.com>
2021-04-13 19:11:37 +0200

    init

---
+++ SB-H/src/main/java/com/example/SBH/SbHApplication.java
@@ -0,0 +1,13 @@
+package com.example.SBH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbHApplication.class, args);
+	}
+
+}
